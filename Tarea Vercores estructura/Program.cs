85ce052c2dae8f0c059f7fc2c117414e90c179c5
KhahoryITLA <20164247@itla.edu.do>
2019-06-05 15:23:12 -0400

    commit

---
+++ Tarea Vercores estructura/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tarea_Vercores_estructura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector = new Vector();
+            vector.ObtenerValor();
+            vector.MostraValor();
+        }
+    }
+}
---
+++ Tarea Vercores estructura/Vector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea_Vercores_estructura
+{
+    class Vector
+    {
+        private int[] numero = new int[9];
+
+        public void ObtenerValor()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Digite el valor del vector numero: " + i);
+                numero[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Listo"); Console.ReadKey();
+        }
+
+        public void MostraValor()
+        {
+            int total = 0;
+            int total2 = 0;
+            foreach (var item in numero)
+            {
+                total = item + total;
+            }
+            Console.WriteLine("El valor acumulado es: " + total);
+            Console.WriteLine("---------------------------------------------------------------");
+            foreach (var item in numero)
+            {
+                if (item >= 36)
+                    total2 = item + total2;
+            }
+            Console.WriteLine("El valor acumulado de los numeros mayores a 36 son: " + total2);
+            Console.WriteLine("---------------------------------------------------------------");
+            foreach (var item in numero)
+            {
+                if (item >= 50)
+                    Console.WriteLine("Los valores mayores a 50 son: " + item);
+            }
+            Console.WriteLine("Listo"); Console.ReadKey();
+        }
+    }
+}
